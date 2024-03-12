35a28700183ad7271d8b2f367896ed69bdb5a952
Bernard_ <bernard.sohl@gmail.com>
2024-03-12 14:43:41 +0100

    hoi

--- Agent.py
+++ Agent.py
@@ -1,11 +1,8 @@
-class Agent:
-    def __init__(self, policy, n_actions, gamma, device):
-        self.policy = policy
-        self.n_actions = n_actions
-        self.gamma = gamma
-        self.device = device
-        self.t_step = 0 #hold current episode timestep
+import pytorch as torch
 
-    def select_action(self, s, policy=None, epsilon=None, temp=None):
+class Agent():
 
+    def __init__(self, layers_size, lr, ):
+        self.lr = lr
 
+    def set_nn(self, layers):
