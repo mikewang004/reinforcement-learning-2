e20fa808893d7643effc315db64071e6b0f5f2c3
Bryce Benz <brycebenzz@gmail.com>
2024-03-12 11:47:57 +0100

    yea2

--- RL ASS 2.py
+++ RL ASS 2.py
@@ -1,1 +1,16 @@
-"""yea"""
+import numpy as np
+import matplotlib.pyplot as plt
+import gymnasium as gym
+gym.make('CartPole-v1')
+
+import gymnasium as gym
+env = gym.make("CartPole-v1")
+
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+env.close()
