770a88cbb3b4dfd98636c6d740086fac31fb9d7f
Bernard_ <99722688+XblaDeZ@users.noreply.github.com>
2024-03-15 14:12:41 +0000

    hoi

--- Agent.py
+++ Agent.py
@@ -2,7 +2,4 @@ import pytorch as torch
 
 class Agent():
 
-    def __init__(self, layers_size, lr, ):
-        self.lr = lr
-
-    def set_nn(self, layers):
+    def __init__(self, seed, layer_sizes, )
