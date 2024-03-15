f8fcb1ba3157e994d9d9753e20f8934385c587c0
Bernard_ <99722688+XblaDeZ@users.noreply.github.com>
2024-03-15 14:48:53 +0000

    begin van agent.py

--- Agent.py
+++ Agent.py
@@ -1,5 +1,18 @@
-import pytorch as torch
-
+import torch
+from torch import nn
+import copy
 class Agent():
 
-    def __init__(self, seed, layer_sizes, )
+    def __init__(self, seed, layer_sizes, lr, sync_frequency, rep_buffer_depth, gamma):
+        torch.manual_seed(seed)
+        self.q_network = self.create_nn(layer_sizes)
+        self.target_network = copy.deepcopy(self.q_network)
+
+    def create_nn(self, layers):
+        assert len(layer) > 1
+        layers = []
+        for i in range(len(layers)-1):
+            linear = nn.Linear(layers[i], layers[i+1])
+            activation = nn.tanh() if i < len(layers)-2 else nn.Identity()
+            layers += [linear, activation]
+        return nn.Sequential(*layers)
