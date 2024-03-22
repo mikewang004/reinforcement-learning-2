87294c5f05aef8b47710cf96c2f5db613a0a0ae2
brycebenz <brycebenzz@gmail.com>
2024-03-22 15:55:30 +0100

    yea

--- Agent.py
+++ Agent.py
@@ -34,14 +34,27 @@ class QNetwork(nn.Module):
         x = torch.relu(self.layer2(x))
         return self.layer3(x)
 
-def select_action(state, steps_done, eps_start, eps_end, eps_decay, env, policy_network, device):
-    sample = random.random()
-    eps_threshold = eps_end + (eps_start - eps_end) * math.exp(-1. * steps_done / eps_decay)
-    if sample > eps_threshold:
-        with torch.no_grad():
-            return policy_network(state).max(1).indices.view(1, 1)
+def select_action(state, steps_done, eps_start, eps_end, eps_decay, env, policy_network, device, policy, temp):
+    if policy == "egreedy":
+        sample = random.random()
+        eps_threshold = eps_end + (eps_start - eps_end) * math.exp(-1. * steps_done / eps_decay)
+        if sample > eps_threshold:
+            with torch.no_grad():
+                return policy_network(state).max(1).indices.view(1, 1)
+        else:
+            return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
+
+    elif policy == "softmax":
+        x = policy_network(state)[0].cpu().detach().numpy()
+        x = np.array(x) / temp  # scale by temperature
+        z = np.array(x) - max(x)  # subtract max to prevent overflow of softmax
+        distr = np.exp(z) / np.sum(np.exp(z))  # compute softmax
+
+        selected_action = np.random.choice([0, 1], 1, p=distr)[0]
+        return torch.tensor([[selected_action]], device=device, dtype=torch.long)
+
     else:
-        return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
+        raise KeyError("Choose either 'egreedy' or 'softmax'")
 
 def train_model(memory, policy_network, target_network, optimizer, device, batch_size, gamma):
     if len(memory) < batch_size:
@@ -74,7 +87,7 @@ def train_model(memory, policy_network, target_network, optimizer, device, batch
     torch.nn.utils.clip_grad_value_(policy_network.parameters(), 100)
     optimizer.step()
 
-def train(env, device, num_episodes, buffer_depth, batch_size, gamma, eps_start, eps_end, eps_decay, tau, lr):
+def train(env, device, num_episodes, buffer_depth, batch_size, gamma, eps_start, eps_end, eps_decay, tau, lr, policy):
     n_actions = env.action_space.n
     state, _ = env.reset()
     n_observations = len(state)
@@ -94,7 +107,7 @@ def train(env, device, num_episodes, buffer_depth, batch_size, gamma, eps_start,
         state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
 
         for t in count():
-            action = select_action(state, steps_done, eps_start, eps_end, eps_decay, env, policy_network, device)
+            action = select_action(state, steps_done, eps_start, eps_end, eps_decay, env, policy_network, device, policy, temp)
             observation, reward, terminated, truncated, _ = env.step(action.item())
             reward = torch.tensor([reward], device=device)
             done = terminated or truncated
@@ -143,7 +156,9 @@ def main():
         eps_end=0.05,
         eps_decay=1000,
         tau=0.005,
-        lr=1e-4
+        lr=1e-4,
+        policy="softmax",
+        temp=0.1
     )
 
 if __name__ == "__main__":
